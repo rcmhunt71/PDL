ab34bf431fb5254c390be2fabe8c217ff5de64ab
Christopher <rcmhunt71@gmail.com>
2018-10-07 20:52:33 -0500

    Added CLI URL processing   * Added URL validation routine (simple validation) + tests   * Added list split and update (for concatenated URLs) + tests

---
+++ configuration/urls.py
@@ -0,0 +1,90 @@
+import pprint
+
+class CliArgProcessing(object):
+
+    REDUCED_LIST = 'reduced_list'
+    TOTAL_DUP_LIST = 'total_dup_list'
+    UNIQUE_DUP_LIST = 'unique_dup_list'
+    DELIMITER = 'https://'
+
+    @classmethod
+    def reduce_url_list(cls, url_list):
+        """
+        Remove any duplicates from the list. Also able to report
+        all duplicates found (as debug).
+        :return: dictionary of:
+            * reduced_list: List of unique elements
+            * total_dup_list: List of all duplicates
+            * unique_dup_list: List of all unique duplicates
+
+        """
+        results = dict()
+        results[cls.REDUCED_LIST] = list(set(url_list))
+        results[cls.TOTAL_DUP_LIST] = [url for n, url in enumerate(url_list) if url in url_list[:n]]
+        results[cls.UNIQUE_DUP_LIST] = list(set(results[cls.TOTAL_DUP_LIST]))
+        return results
+
+    @classmethod
+    def split_urls(cls, url_list, delimiter=DELIMITER):
+        """
+        Check for URLs that are not space delimited from the CLI. If found, split the
+        URL into two URLs and add to the list.
+        :param url_list:
+        :param delimiter:
+        :return: List of valid URLs
+
+        """
+        debug = False
+        if debug:
+            print("List:\n{0}\n\n".format(pprint.pformat(url_list)))
+
+        # Concatenate all entries into a space-delimited string
+        url_concat = ' '.join(url_list)
+
+        # Split on delimiter (Catches concatenated http entries)
+        # Rejoin with space-delimiting (concatenated entries are now separate)
+        url_temp = ' {0}'.format(delimiter).join(url_concat.split(delimiter))
+        if debug and url_concat != url_temp:
+            print("Original URL: {0}".format(url_concat))
+            print("Updated URL: {0}".format(url_temp))
+
+        new_url_list = []
+        for url in url_temp.split(' '):
+            if CliArgProcessing.validate_url(url):
+                new_url_list.append(url)
+
+        return new_url_list
+
+    @classmethod
+    def validate_url(cls, url, delimiter=DELIMITER):
+        """
+        Verify URL starts with delimited, and has additional URL info.
+
+        TODO: Check for domain. <protocol>://<domain>/[[resources]..]
+        :param url: URL
+        :param delimiter: protocol prefix for URL
+        :return: Boolean; True = Valid URL
+
+        """
+
+        # TODO: In place until debug flag and logging is in place
+        debug = False
+        if debug:
+            print("URL: {url}".format(url=url))
+            print("Delimiter: {delimiter}".format(delimiter=delimiter))
+
+        valid = url.lower().startswith(delimiter.lower()) and url.lower() != delimiter.lower()
+        return valid
+
+    @classmethod
+    def list_urls(cls, url_list):
+        """
+        Generate multi-line string of URLs in list
+        :param url_list: List of URLs
+        :return: multi-line string
+
+        """
+        url_list_string = ''
+        for index, url in enumerate(url_list):
+            url_list_string += "{counter}) {curr_url}\n".format(counter=index + 1, curr_url=url)
+        return url_list_string
---
+++ test/configuration/test_urls.py
@@ -0,0 +1,152 @@
+from PDL.configuration.urls import CliArgProcessing
+
+from nose.tools import assert_equals, assert_not_equals, assert_true, assert_false
+
+
+class TestUrlProcessing(object):
+
+    # ===============================================================
+    # -------   CliArgProcessing::reduce_url_list(url_list)   -------
+    # ===============================================================
+
+    UNIQUE_URL_LIST = [1, 2, 3, 4]
+
+    def test_unique_list_with_no_dups(self):
+        expected_unique_dups = 0
+        results = CliArgProcessing.reduce_url_list(url_list=self.UNIQUE_URL_LIST)
+
+        # Unique List
+        assert_equals(len(self.UNIQUE_URL_LIST), len(results[CliArgProcessing.REDUCED_LIST]))
+
+        # Total Duplicates
+        assert_equals(len(results[CliArgProcessing.TOTAL_DUP_LIST]), expected_unique_dups)
+
+        # Unique Duplicates
+        assert_equals(len(results[CliArgProcessing.UNIQUE_DUP_LIST]), expected_unique_dups)
+
+    def test_unique_list_with_single_dups(self):
+        # Create a list of duplicate elements (unique * 2)
+        dup_list = self.UNIQUE_URL_LIST.copy()
+        dup_list.extend(self.UNIQUE_URL_LIST)
+
+        # Expected number of duplicates
+        expected_unique_dups = len(self.UNIQUE_URL_LIST)
+        results = CliArgProcessing.reduce_url_list(url_list=dup_list)
+
+        # Unique List
+        assert_equals(len(results[CliArgProcessing.REDUCED_LIST]), len(self.UNIQUE_URL_LIST))
+
+        # Total Duplicates
+        assert_equals(len(results[CliArgProcessing.TOTAL_DUP_LIST]), expected_unique_dups)
+
+        # Unique Duplicates
+        assert_equals(len(results[CliArgProcessing.UNIQUE_DUP_LIST]), expected_unique_dups)
+
+    def test_unique_list_with_multiple_dups(self):
+        # Create a list of duplicate elements (unique * 3)
+        dup_list = self.UNIQUE_URL_LIST.copy()
+        dup_list.extend(self.UNIQUE_URL_LIST)
+        dup_list.extend(self.UNIQUE_URL_LIST)
+
+        # Expected number of duplicates
+        expected_unique_dups = len(self.UNIQUE_URL_LIST)
+        results = CliArgProcessing.reduce_url_list(url_list=dup_list)
+
+        # Unique List
+        assert_equals(len(results[CliArgProcessing.REDUCED_LIST]), len(self.UNIQUE_URL_LIST))
+
+        # Total Duplicates (two of each unique value)
+        assert_equals(len(results[CliArgProcessing.TOTAL_DUP_LIST]), expected_unique_dups * 2)
+
+        # Unique Duplicates
+        assert_equals(len(results[CliArgProcessing.UNIQUE_DUP_LIST]), expected_unique_dups)
+
+    # ================================================================
+    # ------- CliArgProcessing::validate_url(url, [delimiter]) -------
+    # ================================================================
+
+    VALID_DELIMITER = 'https://'
+    VALID_URL_FORMAT = 'https://test.domain.dom/resource_1/resource_2/endpoint_{0}.jpg'
+    VALID_URL = VALID_URL_FORMAT.format('')
+
+    def test_validate_url_with_valid_url_lowercase(self):
+        url = self.VALID_URL
+        valid = CliArgProcessing.validate_url(url=url)
+        assert_true(valid, "Valid URL ({url}) was marked as invalid.".format(url=url))
+
+    def test_validate_url_with_valid_url_uppercase(self):
+        url = self.VALID_URL.upper()
+        valid = CliArgProcessing.validate_url(url=url)
+        assert_true(valid, "Valid URL ({url}) was marked as invalid.".format(url=url))
+
+    def test_validate_url_with_valid_url_mixed_case(self):
+        url = self.VALID_URL.capitalize()
+        valid = CliArgProcessing.validate_url(url=url)
+        assert_true(valid, "Valid URL ({url}) was marked as invalid.".format(url=url))
+
+    def test_validate_url_with_delimiter_lowercase(self):
+        url = self.VALID_URL
+        delimiter = self.VALID_DELIMITER.lower()
+        valid = CliArgProcessing.validate_url(url=url, delimiter=delimiter)
+        assert_true(valid, "Valid URL ({url}) was marked as invalid with a valid delimiter ({delim}).".format(
+            url=url, delim=delimiter))
+
+    def test_validate_url_with_delimiter_uppercase(self):
+        url = self.VALID_URL
+        delimiter = self.VALID_DELIMITER.upper()
+        valid = CliArgProcessing.validate_url(url=url, delimiter=delimiter)
+        assert_true(valid, "Valid URL ({url}) was marked as invalid with valid delimiter ({delim}).".format(
+            url=url, delim=delimiter))
+
+    def test_validate_url_with_delimiter_mixed_case(self):
+        url = self.VALID_URL
+        delimiter = self.VALID_DELIMITER.capitalize()
+        valid = CliArgProcessing.validate_url(url=url, delimiter=delimiter)
+        assert_true(valid, "Valid URL ({url}) was marked as invalid with valid delimiter ({delim}).".format(
+            url=url, delim=delimiter))
+
+    def test_validate_url_with_invalid_url(self):
+        url = '.idea'
+        valid = CliArgProcessing.validate_url(url=url)
+        assert_false(valid, "Invalid URL ({url}) was marked as valid.".format(url=url))
+
+    def test_validate_url_with_invalid_delimiter(self):
+        url = self.VALID_URL
+        delimiter = "\r\ninvalid"
+        valid = CliArgProcessing.validate_url(url=url, delimiter=delimiter)
+        assert_false(valid, "Valid URL ({url}) was marked as valid with invalid delimiter ({delim}).".format(
+            url=url, delim=delimiter))
+
+    def test_validate_url_with_delimiter_as_url(self):
+        url = self.VALID_DELIMITER
+        delimiter = self.VALID_DELIMITER
+        valid = CliArgProcessing.validate_url(url=url, delimiter=delimiter)
+        assert_false(valid, "Invalid URL ({url}) was marked as valid with valid delimiter ({delim}).".format(
+            url=url, delim=delimiter))
+
+    # ================================================================
+    # ------- CliArgProcessing::split_urls(url_list, [delimiter]) -------
+    # ================================================================
+
+    def test_split_urls_all_elems_valid_urls(self):
+        valid_split_url_list = [self.VALID_URL_FORMAT.format(id_) for id_ in range(1, 10)]
+        updated_list = CliArgProcessing.split_urls(valid_split_url_list)
+        assert_equals(len(valid_split_url_list), len(updated_list))
+
+    def test_split_urls_list_concatenated_into_single_element(self):
+        list_size = 10
+        valid_split_url_list = [''.join([self.VALID_URL_FORMAT.format(id_) for id_ in range(0, list_size)])]
+        updated_list = CliArgProcessing.split_urls(valid_split_url_list)
+        assert_not_equals(len(valid_split_url_list), len(updated_list))
+        assert_equals(len(updated_list), list_size)
+
+    def test_split_urls_with_concatenated_element(self):
+        list_size = 10
+        valid_split_url_list = [self.VALID_URL_FORMAT.format(id_) for id_ in range(1, 10)]
+
+        valid_split_url_list.append(valid_split_url_list[-1] + valid_split_url_list[-2] + valid_split_url_list[-3])
+        list_size += 2
+
+        updated_list = CliArgProcessing.split_urls(valid_split_url_list)
+        assert_not_equals(len(valid_split_url_list), len(updated_list))
+        assert_equals(len(updated_list), list_size)
