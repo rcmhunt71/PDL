4068ac9e45c2cf44186ab457c74383ea427ca361
Christopher Hunt <chris.hunt@RACKSPACE.COM>
2018-10-08 18:21:46 -0500

    Added logging preamble logic   * Provides context info about the invoking call.   * Supports additional levels of logging routines (e.g. additional logging routine to call log.info())   * Quick module level visual test

--- logging/logger.py
+++ logging/logger.py
@@ -1,14 +1,19 @@
+import inspect
 import logging
+import os
 
 
 class Logger(object):
 
-    LOG_FORMAT = r'%(asctime)s: %(name)s:%(module)s:%(funcName)s:%(lineno)d: %(levelname)s - %(message)s'
+    LOG_FORMAT = r'[%(asctime)-15s] [%(levelname)-5s] [%(name)s] [%(file_name)s:%(routine)s:%(linenum)d] - %(message)s'
     DATE_FORMAT = r'%m%d%y-%T'
 
-    def __init__(self, filename=None, default_level=logging.INFO):
+    DEFAULT_DEPTH = 3
+
+    def __init__(self, filename=None, default_level=logging.INFO, added_depth=0):
         self.filename = filename
         self.loglevel = default_level
+        self.depth = self.DEFAULT_DEPTH + int(added_depth)
 
         self.logger = None
         self._start_logger()
@@ -29,10 +34,42 @@ class Logger(object):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(self.loglevel)
 
+    def _log_level(self, level, msg, prefix=''):
+        log_routine = getattr(self.logger, level.lower())
+        log_routine(str(prefix) + str(msg), extra=self._method())
+
+    def _method(self):
+        frame_info = inspect.stack()[self.depth]
+
+        return {'file_name': frame_info[1],
+                'linenum': frame_info[2],
+                'routine': frame_info[3]}
+
+    def fatal(self, msg):
+        self._log_level(level='FATAL', msg=msg)
+
+    def error(self, msg):
+        self._log_level(level='ERROR', msg=msg)
+
+    def warn(self, msg):
+        self._log_level(level='WARN', msg=msg)
+
     def info(self, msg):
-        self.logger.info(msg)
+        self._log_level(level='INFO', msg=msg)
+
+    def debug(self, msg):
+        self._log_level(level='DEBUG', msg=msg)
 
 
 if __name__ == '__main__':
-    log = Logger(default_level=logging.INFO)
-    log.info('TEST')
+
+    def test_routine(logger, level, msg):
+        testlog = getattr(logger, level.lower())
+        testlog(msg)
+
+    def wooba():
+        test_routine(log, 'info', 'TEST')
+        test_routine(log, 'debug', 'TEST2')
+
+    log = Logger(default_level=logging.DEBUG, added_depth=1)
+    wooba()
