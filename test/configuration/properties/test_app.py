3f7313bff9b397ce12e24fd17acb30b08b33e4c6
Christopher Hunt <chris.hunt@RACKSPACE.COM>
2018-10-16 08:14:27 -0500

    Refactored app_cfg to inherit and extend ConfigParser   * Rather than keep a ConfigParser instance in the class, extend the ConfigParser class, and use ConfigParser methods directly, rather than shims to bubble up.   * Fixed super() call (new format)   * Updated tests   * Reduced required logic (simpler implementation)

--- test/configuration/properties/test_app.py
+++ test/configuration/properties/test_app.py
@@ -1,8 +1,9 @@
+from ConfigParser import NoSectionError, NoOptionError
 import inspect
 import os
 
 from PDL.configuration.properties.app_cfg import (
-    Config, ConfigSectionDoesNotExist, OptionDoesNotExist,
+    AppConfig, ConfigSectionDoesNotExist, OptionDoesNotExist,
     CannotCastValueToType)
 
 from nose.tools import assert_equals, assert_true, raises
@@ -21,12 +22,17 @@ class TestPropertiesConfig(object):
     VALID_SECTION_AND_ITEM_FLOAT = ('test', 'averages', 5.5)
     VALID_SECTION_AND_ITEM_LIST = (
         'teardown', 'resources', ['Nova', 'Neutron', 'Cinder'])
+    INVALID_SECTION_AND_VALID_OPTION_LIST = (
+        INVALID_SECTION, 'resources', ['Nova', 'Neutron', 'Cinder'])
+    VALID_SECTION_AND_INVALID_OPTION_LIST = (
+        'teardown', INVALID_OPTION, ['Nova', 'Neutron', 'Cinder'])
+
 
     @classmethod
     def setup_class(cls):
         filename = inspect.getframeinfo(inspect.currentframe()).filename
         cls.cfg_file = '{0}.data'.format(os.path.splitext(filename)[0])
-        cls.config = Config(cfg_file=cls.cfg_file)
+        cls.config = AppConfig(cfg_file=cls.cfg_file)
 
     def _print_config_file(self):
         with open(self.cfg_file) as CFG:
@@ -37,7 +43,7 @@ class TestPropertiesConfig(object):
     def test_get_sections(self):
         expected_sections = ['setup', 'test', 'teardown']
         self._print_config_file()
-        assert_equals(self.config.get_sections(), expected_sections)
+        assert_equals(self.config.sections(), expected_sections)
 
     def test_get_options_populated(self):
         actual_options = self.config.get_options(section=self.VALID_OPTIONS[0])
@@ -49,24 +55,24 @@ class TestPropertiesConfig(object):
         actual_options = self.config.get_options(section=self.INVALID_SECTION)
         assert_equals(sorted(actual_options), sorted(expected_options))
 
-    @raises(ConfigSectionDoesNotExist)
+    @raises(NoSectionError)
     def test_get_option_non_existent_section(self):
         expected_option = None
-        actual_option = self.config.get_option(
+        actual_option = self.config.get(
             section=self.INVALID_SECTION, option=self.INVALID_OPTION)
         assert_equals(actual_option, expected_option)
 
-    @raises(OptionDoesNotExist)
+    @raises(NoOptionError)
     def test_get_option_non_existent_option(self):
         expected_option = None
-        actual_option = self.config.get_option(
+        actual_option = self.config.get(
             section=self.VALID_SECTIONS[-1], option=self.INVALID_OPTION)
         assert_equals(actual_option, expected_option)
 
     def test_get_option_valid_section_valid_option(self):
         data = self.VALID_SECTION_AND_ITEM_STR
         expected_value = data[2]
-        actual_value = self.config.get_option(
+        actual_value = self.config.get(
             section=data[0], option=data[1])
         assert_true(isinstance(actual_value, str))
         assert_equals(actual_value, expected_value)
@@ -74,7 +80,7 @@ class TestPropertiesConfig(object):
     def test_get_option_valid_section_valid_int(self):
         data = self.VALID_SECTION_AND_ITEM_INT
         expected_value = data[2]
-        actual_value = self.config.get_int(
+        actual_value = self.config.getint(
             section=data[0], option=data[1])
         assert_true(isinstance(actual_value, int))
         assert_equals(actual_value, expected_value)
@@ -82,7 +88,7 @@ class TestPropertiesConfig(object):
     def test_get_option_valid_section_valid_boolean(self):
         data = self.VALID_SECTION_AND_ITEM_BOOL
         expected_value = data[2]
-        actual_value = self.config.get_boolean(
+        actual_value = self.config.getboolean(
             section=data[0], option=data[1])
         assert_true(isinstance(actual_value, bool))
         assert_equals(actual_value, expected_value)
@@ -90,16 +96,16 @@ class TestPropertiesConfig(object):
     def test_get_option_valid_section_valid_float(self):
         data = self.VALID_SECTION_AND_ITEM_FLOAT
         expected_value = data[2]
-        actual_value = self.config.get_float(
+        actual_value = self.config.getfloat(
             section=data[0], option=data[1])
         assert_true(isinstance(actual_value, float))
         assert_equals(actual_value, expected_value)
 
-    @raises(CannotCastValueToType)
+    @raises(ValueError)
     def test_get_option_valid_section_valid_str_illegal_cast_float(self):
         data = self.VALID_SECTION_AND_ITEM_STR
         expected_value = data[2]
-        actual_value = self.config.get_float(
+        actual_value = self.config.getfloat(
             section=data[0], option=data[1])
         assert_true(isinstance(actual_value, float))
         assert_equals(actual_value, expected_value)
@@ -107,7 +113,25 @@ class TestPropertiesConfig(object):
     def test_get_option_valid_section_valid_list(self):
         data = self.VALID_SECTION_AND_ITEM_LIST
         expected_value = data[2]
-        actual_value = self.config.get_list(
+        actual_value = self.config.getlist(
+            section=data[0], option=data[1])
+        assert_true(isinstance(actual_value, list))
+        assert_equals(actual_value, expected_value)
+
+    @raises(NoSectionError)
+    def test_get_option_invalid_section_valid_option_valid_list(self):
+        data = self.INVALID_SECTION_AND_VALID_OPTION_LIST
+        expected_value = data[2]
+        actual_value = self.config.getlist(
+            section=data[0], option=data[1])
+        assert_true(isinstance(actual_value, list))
+        assert_equals(actual_value, expected_value)
+
+    @raises(NoOptionError)
+    def test_get_option_valid_section_invalid_option_valid_list(self):
+        data = self.VALID_SECTION_AND_INVALID_OPTION_LIST
+        expected_value = data[2]
+        actual_value = self.config.getlist(
             section=data[0], option=data[1])
         assert_true(isinstance(actual_value, list))
         assert_equals(actual_value, expected_value)
