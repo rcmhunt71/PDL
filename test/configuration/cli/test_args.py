f630fbe055c65758b074f331fe3d03c7629b9a01
Christopher Hunt <chris.hunt@RACKSPACE.COM>
2018-10-19 15:02:12 -0500

    Cleaned up extraneous code * Removed TODO (completed with last commit) * Fixed missed hardcodes * Converted --{0}.format(attr) to a routine: _build_longword_option(option) * Moved all non-test routines to the end of the file

--- test/configuration/cli/test_args.py
+++ test/configuration/cli/test_args.py
@@ -1,18 +1,18 @@
 from PDL.configuration.cli.args import CLIArgs, ArgSubmodules, ArgOptions
-
 from nose.tools import raises, assert_equals, assert_false, assert_true
 
 
 class TestCommandLine(object):
 
-    # TODO: Add test case descriptions
+
+
 
     def test_if_debug_flag_is_set(self):
         """
         Validate debug flag is reported as set.
         """
         attr = ArgOptions.DEBUG
-        cli_args = ['--{0}'.format(attr), ArgSubmodules.DOWNLOAD]
+        cli_args = [self._build_longword_option(attr), ArgSubmodules.DOWNLOAD]
         expectation = True
         self._verify_boolean_response(
             attr=attr, cli_args=cli_args, bool_expectation=expectation)
@@ -32,7 +32,7 @@ class TestCommandLine(object):
         Validate dryrun flag is reported as set.
         """
         attr = ArgOptions.DRYRUN
-        cli_args = ['--{0}'.format(attr), ArgSubmodules.DOWNLOAD]
+        cli_args = [self._build_longword_option(attr), ArgSubmodules.DOWNLOAD]
         expectation = True
         self._verify_boolean_response(
             attr=attr, cli_args=cli_args, bool_expectation=expectation)
@@ -46,26 +46,6 @@ class TestCommandLine(object):
         expectation = False
         self._verify_boolean_response(
             attr=attr, cli_args=cli_args, bool_expectation=expectation)
-
-    @staticmethod
-    def _verify_boolean_response(attr, cli_args, bool_expectation):
-        """
-        Sets up CLIArgs object with provided args, verify the boolean response
-        is correct, based on the boolean expectation.
-
-        :param attr: Option to test (created as an attribute of CLIArgs)
-        :param cli_args: CLI arguments
-        :param bool_expectation: Boolean T/F
-        :return: assert or pass
-
-        """
-        cli = CLIArgs(test_args_list=cli_args)
-        attribute = getattr(cli.args, attr)
-        print("{attr} ATTRIBUTE: {val}".format(attr=attr, val=attribute))
-        if bool_expectation:
-            assert_true(attribute)
-        else:
-            assert_false(attribute)
 
     def test_if_command_attribute_is_set_to_submenu(self):
         """
@@ -91,7 +71,7 @@ class TestCommandLine(object):
         # Actual arg list: --cfg
 
         attr = ArgOptions.CFG
-        cli = CLIArgs(test_args_list=['--{0}'.format(attr)])
+        cli = CLIArgs(test_args_list=[self._build_longword_option(attr)])
         attribute = getattr(cli.args, attr)
         print("{attr} ATTRIBUTE: {val}".format(attr=attr, val=attribute))
 
@@ -104,7 +84,8 @@ class TestCommandLine(object):
         # Actual arg list: info image
 
         attr = ArgOptions.IMAGE
-        cli = CLIArgs(test_args_list=[ArgSubmodules.INFO, '--{0}'.format(attr)])
+        cli = CLIArgs(test_args_list=[ArgSubmodules.INFO,
+                                      self._build_longword_option(attr)])
         attribute = getattr(cli.args, attr)
         print("{attr} ATTRIBUTE: {val}".format(attr=attr, val=attribute))
 
@@ -114,7 +95,8 @@ class TestCommandLine(object):
         Test if db --sync and --records are mutually exclusive.
         """
         attr = [ArgOptions.SYNC, ArgOptions.RECORDS]
-        cli = CLIArgs(test_args_list=['--{0}'.format(attr[0]), '--{0}'.format(attr[1])])
+        cli = CLIArgs(test_args_list=[self._build_longword_option(attr[0]),
+                                      self._build_longword_option(attr[1])])
         for attribute in attr:
             value = getattr(cli.args, attribute)
             print("{attr} ATTRIBUTE: {val}".format(attr=attribute, val=value))
@@ -125,7 +107,8 @@ class TestCommandLine(object):
         """
         attr = ArgOptions.SYNC
         designator = ArgSubmodules.DATABASE
-        cli = CLIArgs(test_args_list=[designator, '--{0}'.format(attr)])
+        cli = CLIArgs(
+            test_args_list=[designator, self._build_longword_option(attr)])
         value = getattr(cli.args, attr)
         print("{attr} ATTRIBUTE: {val}".format(attr=attr, val=value))
         assert_equals(getattr(cli.args, ArgOptions.COMMAND), designator)
@@ -137,7 +120,8 @@ class TestCommandLine(object):
         """
         attr = ArgOptions.DETAILS
         designator = ArgSubmodules.DATABASE
-        cli = CLIArgs(test_args_list=[designator, '--{0}'.format(attr)])
+        cli = CLIArgs(test_args_list=[designator,
+                                      self._build_longword_option(attr)])
         value = getattr(cli.args, attr)
         print("{attr} ATTRIBUTE: {val}".format(attr=attr, val=value))
         assert_equals(getattr(cli.args, ArgOptions.COMMAND), designator)
@@ -169,21 +153,21 @@ class TestCommandLine(object):
         attributes = ['records', 'details', filespec_opt]
         designator = ArgSubmodules.DATABASE
         args = [designator]
-        args.extend(['--{filespec_opt}'.format(filespec_opt=filespec_opt),
-                     '{filespec}'.format(filespec=filespec)])
-        args.extend(['--{0}'.format(attr) for attr in attributes if
+        args.extend([self._build_longword_option(filespec_opt), filespec])
+        args.extend([self._build_longword_option(attr) for attr in attributes if
                      attr != filespec_opt])
 
         cli = CLIArgs(test_args_list=args)
 
-        assert_equals(getattr(cli.args, 'command'), designator)
+        assert_equals(getattr(cli.args, ArgOptions.COMMAND), designator)
         for attr in attributes:
             value = getattr(cli.args, attr)
-            print("{attr} ATTRIBUTE: {val}".format(attr=attr, val=value))
+            print("{attr} ATTRIBUTE: {name} = {val}".format(
+                attr=attr.upper(), name=attr, val=value))
+            expected = True
             if attr == filespec_opt:
-                assert_equals(getattr(cli.args, attr), filespec)
-            else:
-                assert_equals(getattr(cli.args, attr), True)
+                expected = filespec
+            assert_equals(getattr(cli.args, attr), expected)
 
     @raises(SystemExit)
     def test_if_db_record_details_and_filespec_opts_wo_filespecs_errors(self):
@@ -197,7 +181,7 @@ class TestCommandLine(object):
                       ArgOptions.FILESPEC]
         designator = ArgSubmodules.DATABASE
         args = [designator]
-        args.extend(['--{0}'.format(attr) for attr in attributes])
+        args.extend([self._build_longword_option(attr) for attr in attributes])
 
         cli = CLIArgs(test_args_list=args)
 
@@ -219,7 +203,8 @@ class TestCommandLine(object):
         # Actual arg list: <option> <value>
 
         attr = ArgOptions.IMAGE
-        cli = CLIArgs(test_args_list=['--{0}'.format(attr), 'foo.tmp'])
+        cli = CLIArgs(test_args_list=[self._build_longword_option(attr),
+                                      'foo.tmp'])
         attribute = getattr(cli.args, attr)
         print("{attr} ATTRIBUTE: {val}".format(attr=attr, val=attribute))
 
@@ -233,3 +218,27 @@ class TestCommandLine(object):
 
         designator = 'FOO'
         CLIArgs(test_args_list=[designator])
+
+    @staticmethod
+    def _verify_boolean_response(attr, cli_args, bool_expectation):
+        """
+        Sets up CLIArgs object with provided args, verify the boolean response
+        is correct, based on the boolean expectation.
+
+        :param attr: Option to test (created as an attribute of CLIArgs)
+        :param cli_args: CLI arguments
+        :param bool_expectation: Boolean T/F
+        :return: assert or pass
+
+        """
+        cli = CLIArgs(test_args_list=cli_args)
+        attribute = getattr(cli.args, attr)
+        print("{attr} ATTRIBUTE: {val}".format(attr=attr, val=attribute))
+        if bool_expectation:
+            assert_true(attribute)
+        else:
+            assert_false(attribute)
+
+    @staticmethod
+    def _build_longword_option(option):
+        return '--{option}'.format(option=option)
