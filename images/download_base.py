7a7ebe49d1fa9dafd26ad4dfae23518dadcc624c
Christopher Hunt <chris.hunt@RACKSPACE.COM>
2018-10-22 17:17:30 -0500

    Basic layout of base classes for page parsing

---
+++ images/download_base.py
@@ -0,0 +1,50 @@
+
+class ParsePage(object):
+    """ Generic object for tracking page parsing actions. """
+
+    def __init__(self, page_url):
+        self.page_url = page_url
+        self.page_contents = None
+        self.image_pages = list()
+
+    def get_page(self):
+        raise NotImplemented
+
+    def parse_page(self):
+        raise NotImplemented
+
+
+class CatalogPage(ParsePage):
+    """
+    Used when a series of contact sheets are listed: catalog of groups of images
+    Return list of ImageContactPage objects
+    """
+
+    pass
+
+
+class ImageContactPage(ParsePage):
+    """
+    Used to determine metadata and URL of image(s) parsed from page.
+    Return: List of ImageData objects
+    """
+
+    def __init__(self, page_url):
+        super(ParsePage).__init__(page_url=page_url)
+        self.image_urls = list()
+
+
+class ImageStatus(object):
+    def __init__(self, image_list):
+        self.image_list = image_list
+
+    def does_image_exist(self):
+        raise NotImplemented
+
+
+class DownloadImages(object):
+    def __init__(self, image_list):
+        self.image_list = image_list
+
+    def download_image(self):
+        raise NotImplemented
