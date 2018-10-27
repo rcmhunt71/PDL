892095e8f64583ccf2adb08a78a6c8c21f921611
Christopher Hunt <chris.hunt@RACKSPACE.COM>
2018-10-26 23:06:37 -0500

    Added unittests for module_imports module.

---
+++ test/engine/class_test.py
@@ -0,0 +1,6 @@
+class ClassFoo(object):
+    def __init__(self, foo='bar'):
+        self.name = foo
+
+    def show_me_the_name(self):
+        return self.name
---
+++ test/engine/test_module_imports.py
@@ -0,0 +1,27 @@
+import sys
+
+import PDL.engine.module_imports as imports
+
+from nose.tools import assert_equals, assert_not_equals, assert_true, assert_false
+
+
+class TestModuleImports(object):
+
+    def test_does_existing_module_loads(self):
+        module_to_load = "PDL.test.engine.class_test"
+        module_not_loaded = "json"
+
+        imports.import_module(module_to_load)
+        for x in sorted(sys.modules.keys()):
+            print(x)
+        assert module_to_load in sys.modules
+
+    def test_does_existing_class_loads(self):
+        test_value = "test"
+        module_to_load = "PDL.test.engine.class_test.ClassFoo"
+
+        test_class = imports.import_module_class(module_to_load)
+        test_instance = test_class(test_value)
+
+        assert isinstance(test_instance, test_class)
+        assert_equals(test_instance.name, test_value)
