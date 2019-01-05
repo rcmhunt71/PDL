45c2ec4bae0966baef565c087ff3af2526e333a2
Christopher <rcmhunt71@gmail.com>
2019-01-05 16:22:51 -0600

    Updated ImageData class to build objects from dict * Added class level vars for defining & listing image-specific metadata * Added __repr__() and __str__() methods. * Added docstrings

--- engine/images/image_info.py
+++ engine/images/image_info.py
@@ -1,10 +1,29 @@
+from pprint import pformat
+
 from PDL.engine.images.status import (
     DownloadStatus as Status,
     ImageDataModificationStatus as ModStatus
 )
+from PDL.logger.logger import Logger
+
+log = Logger()
 
 
 class ImageData(object):
+    """ Image Metadata Storage Object """
+
+    # Used for checking specific attributes that would not change for the image.
+    DL_STATUS = "dl_status"
+    IMAGE_NAME = "image_name"
+    PAGE_URL = 'page_url'
+    IMAGE_URL = 'image_url'
+    AUTHOR = 'author'
+    DESCRIPTION = 'description'
+    RESOLUTION = 'resolution'
+
+    # List of the metadata specific to the image
+    METADATA = [DL_STATUS, IMAGE_NAME, PAGE_URL, IMAGE_URL, AUTHOR, DESCRIPTION, RESOLUTION]
+
     def __init__(self):
         self.image_name = None
         self.description = None
@@ -21,11 +40,50 @@ class ImageData(object):
         self.mod_status = ModStatus.MOD_NOT_SET
         self.error_info = None
 
+    def __str__(self):
+        return pformat(self.to_dict())
+
+    def __repr__(self):
+        return self.__str__()
+
     def to_dict(self):
+        """
+        Convert the object to a dictionary representation.
+
+        :return: dictionary representation of the object.
+
+        """
         attributes = self._list_attributes()
         attr_dict = dict([(key, getattr(self, key)) for key in attributes])
         return attr_dict
 
     def _list_attributes(self):
+        """
+        List of all object attributes:
+          * not callable,
+          * no capital letters (PEP-8)
+          * does not start with '_' ==> internal attributes
+
+        :return: list of all object attributes
+        """
         return [x for x in dir(self) if not x.startswith('_')
                 and x.lower() == x and not callable(getattr(self, x))]
+
+    @classmethod
+    def build_obj(cls, dictionary):
+        """
+        Builds an object from a dictionary implementation
+        :param dictionary: Dictionary representation of the ImageData object
+
+        :return: Instantiated, populated ImageData object
+
+        """
+        attr_err_msg = "Unrecognized ImageData attribute: '{attr}' , value: {val}"
+
+        obj = ImageData()
+        for key, value in dictionary.items():
+            if hasattr(obj, key):
+                setattr(obj, key, value)
+            else:
+                log.error(attr_err_msg.format(attr=key, val=value))
+        return obj
