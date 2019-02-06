3c4bf9a5b180e351a25c5f89ff0af9f1a28266c2
robe6162 <chris.hunt@rackspace.com>
2019-02-06 14:46:37 -0600

    Added static typing to arguments and return values

--- test/utils/test_utils.py
+++ test/utils/test_utils.py
@@ -2,9 +2,9 @@ import os
 import uuid
 
 
-def get_truncated_uuid():
+def get_truncated_uuid() -> str:
     return str(uuid.uuid4()).split('-')[-1]
 
 
-def get_pid():
+def get_pid() -> int:
     return os.getpid()
