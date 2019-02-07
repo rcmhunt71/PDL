317ed9fb89d0a035294405644750dd439e620a27
robe6162 <chris.hunt@rackspace.com>
2019-02-07 16:24:47 -0600

    Addressed TODOs: * Added docstrings * Corrected static parameter types --> Callable

--- engine/module_imports.py
+++ engine/module_imports.py
@@ -1,5 +1,6 @@
 import importlib
 import inspect
+from typing import Callable
 
 from PDL.logger.logger import Logger
 
@@ -11,10 +12,35 @@ class ClassNotFoundInImportedModule(Exception):
         self.message = f"Class '{klass}' not found in module {module}"
 
 
-# TODO: Add docstrings
-# TODO: Look up and add return type of unknown module
+"""
+This module provides the basic functionality to take a given dotted path (str) to a module or class,
+and import the module/class. This allows a user to specify which classes to import for use
+within the application. 
 
-def import_module(dotted_path_module: str):
+Application Use Case:
+For a given use, the modules have the same base class, so there are well defined/identified
+interfaces, and the imported class is assigned to a generic variable that can used.
+
+   Example:
+   ========
+       target_class_a = project.foo.module.ClassA
+       target_class_b = project.foo.module.ClassB
+
+       for klass in [target_class_a, target_class_b]:
+          MyClassDef = import_module_class(klass)
+          my_class = MyClassDef(<params>)
+          my_class.method1()
+          print my_class.method2()  
+
+ 
+Both classes have been dynamically loaded, instantiated, and methods invoked by generalization.
+As long as each imported class contains at least the same invoked methods (built-in assumption), 
+any class can be called/utilized.
+ 
+"""
+
+
+def import_module(dotted_path_module: str) -> Callable:
     """
     Imports a module into memory.
     :param dotted_path_module: from.here.you.can.find.the.module
@@ -34,9 +60,7 @@ def import_module(dotted_path_module: str):
     return module
 
 
-# TODO: Look up and add return type of unknown class
-
-def import_module_class(dotted_path_class: str):
+def import_module_class(dotted_path_class: str) -> Callable:
     """
     Imports the class from the module. Given a module reference, get the class.
 
@@ -65,12 +89,10 @@ def import_module_class(dotted_path_class: str):
     return getattr(module, klass, None)
 
 
-# TODO: Look up and add return type for trace
-
-def build_stack_trace(trace):
+def build_stack_trace(trace: list):
     stack_fmt = "\n{path}:L{lineno}\n\tROUTINE: {routine}\n\tCODE: {code}\n"
     stack_trace = ''
     for stack in trace:
-        stack_trace += stack_fmt.format(path=stack[1], lineno=stack[2],
-                                        routine=stack[3], code=stack[4][0].strip())
+        stack_trace += stack_fmt.format(
+            path=stack[1], lineno=stack[2], routine=stack[3], code=stack[4][0].strip())
     return stack_trace
