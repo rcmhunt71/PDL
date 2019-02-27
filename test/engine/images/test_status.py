c2b28070cec54f5df1c5a026ae2c444e6f9b5807
Christopher <rcmhunt71@gmail.com>
2019-02-26 23:51:09 -0600

    Abstracted method name to class-level constant, rather than embed in routines.

--- test/engine/images/test_status.py
+++ test/engine/images/test_status.py
@@ -4,21 +4,23 @@ from nose.tools import assert_equals
 
 
 class TestStatuses(object):
-    def test_get_statuses_for_downloadstatus(self):
+
+    TEST_METHOD = 'get_statuses'
+
+    def test_get_statuses_for_downloadstatus_class(self):
         klass = DownloadStatus
         known_statuses = {'NOT_SET', 'PENDING', 'DOWNLOADED', 'IN_DATABASE', 'EXISTS', 'ERROR'}
         self._verify_intersection_is_empty(
             target_set=known_statuses, target_class=klass)
 
-    def test_get_statuses_for_imagedatamodificationstatus(self):
+    def test_get_statuses_for_imagedatamodificationstatus_class(self):
         klass = ImageDataModificationStatus
         known_statuses = {'NEW', 'UPDATED', 'DELETE', 'MOD_NOT_SET', 'UNCHANGED'}
         self._verify_intersection_is_empty(
             target_set=known_statuses, target_class=klass)
 
-    @staticmethod
-    def _verify_intersection_is_empty(target_set, target_class):
-        returned_statuses = set(getattr(target_class, 'get_statuses_')())
+    def _verify_intersection_is_empty(self, target_set, target_class):
+        returned_statuses = set(getattr(target_class, self.TEST_METHOD)())
         print(f"KNOWN STATUSES:\n{target_set}")
         print(f"RETURNED STATUSES:\n{returned_statuses}")
 
